6e2615a1dc338eef05140fc792119a8abcbc8e09
Mike Samuel <mikesamuel@gmail.com>
2016-10-24 12:27:20 -0400

    testcase

---
+++ src/test/java/com/mikesamuel/cil/ast/ExpressionNameNodeTest.java
@@ -0,0 +1,44 @@
+package com.mikesamuel.cil.ast;
+
+import org.junit.Test;
+
+import com.mikesamuel.cil.ptree.PTree;
+import static com.mikesamuel.cil.ast.MatchEvent.content;
+import static com.mikesamuel.cil.ast.MatchEvent.pop;
+import static com.mikesamuel.cil.ast.MatchEvent.push;
+import static com.mikesamuel.cil.ast.MatchEvent.token;
+
+@SuppressWarnings("javadoc")
+public final class ExpressionNameNodeTest extends AbstractParSerTestCase {
+
+  @Test
+  public void testSimpleExpressionName() {
+    assertParsePasses(
+        PTree.complete(NodeType.ExpressionName),
+        "name",
+        push(ExpressionNameNode.Variant.Identifier),
+        push(IdentifierNode.Variant.Builtin),
+        content("name"),
+        pop(),
+        pop());
+  }
+
+  @Test
+  public void testComplexExpressionName() {
+    assertParsePasses(
+        PTree.complete(NodeType.ExpressionName),
+        "one.two",
+        push(ExpressionNameNode.Variant.ExpressionNameDotIdentifier),
+        push(ExpressionNameNode.Variant.Identifier),
+        push(IdentifierNode.Variant.Builtin),
+        content("one"),
+        pop(),
+        pop(),
+        token("."),
+        push(IdentifierNode.Variant.Builtin),
+        content("two"),
+        pop(),
+        pop());
+  }
+
+}
