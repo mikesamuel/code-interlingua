19a541362567b27e67cd6a169e49802cde9c8e3e
Mike Samuel <mikesamuel@gmail.com>
2016-12-31 14:05:28 -0500

    fleshed out ClassMemberPass

--- src/main/java/com/mikesamuel/cil/ast/passes/ClassMemberPass.java
+++ src/main/java/com/mikesamuel/cil/ast/passes/ClassMemberPass.java
@@ -4,7 +4,9 @@ import java.util.logging.Logger;
 
 import javax.annotation.Nullable;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.mikesamuel.cil.ast.ArrayTypeNode;
 import com.mikesamuel.cil.ast.BaseNode;
 import com.mikesamuel.cil.ast.ClassOrInterfaceTypeNode;
@@ -13,9 +15,12 @@ import com.mikesamuel.cil.ast.CompilationUnitNode;
 import com.mikesamuel.cil.ast.ConstantDeclarationNode;
 import com.mikesamuel.cil.ast.FieldDeclarationNode;
 import com.mikesamuel.cil.ast.FloatingPointTypeNode;
+import com.mikesamuel.cil.ast.FormalParameterListNode;
+import com.mikesamuel.cil.ast.FormalParameterNode;
 import com.mikesamuel.cil.ast.IdentifierNode;
 import com.mikesamuel.cil.ast.IntegralTypeNode;
 import com.mikesamuel.cil.ast.InterfaceTypeNode;
+import com.mikesamuel.cil.ast.LastFormalParameterNode;
 import com.mikesamuel.cil.ast.NodeType;
 import com.mikesamuel.cil.ast.NumericTypeNode;
 import com.mikesamuel.cil.ast.PrimitiveTypeNode;
@@ -26,6 +31,7 @@ import com.mikesamuel.cil.ast.TypeVariableNode;
 import com.mikesamuel.cil.ast.UnannTypeNode;
 import com.mikesamuel.cil.ast.VariableDeclaratorIdNode;
 import com.mikesamuel.cil.ast.meta.CallableInfo;
+import com.mikesamuel.cil.ast.meta.FieldInfo;
 import com.mikesamuel.cil.ast.meta.MemberInfo;
 import com.mikesamuel.cil.ast.meta.Name;
 import com.mikesamuel.cil.ast.meta.StaticType;
@@ -34,6 +40,7 @@ import com.mikesamuel.cil.ast.meta.TypeInfo;
 import com.mikesamuel.cil.ast.meta.TypeNameResolver;
 import com.mikesamuel.cil.ast.meta.TypeSpecification;
 import com.mikesamuel.cil.ast.traits.CallableDeclaration;
+import com.mikesamuel.cil.ast.traits.MemberDeclaration;
 import com.mikesamuel.cil.ast.traits.TypeDeclaration;
 import com.mikesamuel.cil.ast.traits.TypeScope;
 import com.mikesamuel.cil.ast.traits.WholeType;
@@ -77,8 +84,95 @@ final class ClassMemberPass extends AbstractPass<Void> {
       CallableInfo info = (CallableInfo) memberWithName(
           childTypeInfo, methodName);
       if (info != null) {
-        // TODO: use parameter types to derive a proper method descriptor and
-        // store it and any return type with the method info.
+        StaticType returnType;
+        if (cd.getMethodName().startsWith("<")) {
+          // A special method.
+          returnType = StaticType.T_VOID;
+        } else {
+          WholeType wholeReturnType = findWholeType(node);
+          returnType = wholeReturnType != null
+              ? wholeReturnType.getStaticType() : null;
+          if (returnType == null) {
+            error(node, "Cannot compute return type");
+            returnType = StaticType.ERROR_TYPE;
+          }
+        }
+
+        FormalParameterListNode paramList = null;
+        for (FormalParameterListNode ps
+            : node.finder(FormalParameterListNode.class)
+            .exclude(NodeType.MethodBody, NodeType.ConstructorBody)
+               .find()) {
+          Preconditions.checkState(paramList == null);
+          paramList = ps;
+        }
+
+        boolean hasErrorType = false;
+        String errorDesc = StaticType.ERROR_TYPE.toDescriptor();
+        StringBuilder descriptor = new StringBuilder();
+
+        if (paramList == null) {
+          descriptor.append("()");
+        } else {
+          descriptor.append('(');
+          for (BaseNode formal
+              : Iterables.concat(
+                  node.finder(FormalParameterNode.class)
+                  .exclude(
+                      NodeType.MethodBody,
+                      NodeType.ConstructorBody, NodeType.BlockStatements)
+                  .find(),
+                  node.finder(LastFormalParameterNode.class)
+                  .exclude(
+                      NodeType.MethodBody,
+                      NodeType.ConstructorBody, NodeType.BlockStatements)
+                  .find())) {
+            boolean isVariadic = false;
+            if (formal instanceof LastFormalParameterNode) {
+              if (LastFormalParameterNode.Variant.Variadic
+                  == formal.getVariant()) {
+                isVariadic = true;
+              } else {
+                continue;
+              }
+            }
+            WholeType formalType = findWholeType(formal);
+            StaticType staticType =
+                formalType != null ? formalType.getStaticType() : null;
+            if (staticType == null) {
+              staticType = StaticType.ERROR_TYPE;
+            }
+            if (isVariadic) {
+              // Promote T... formals to arrays.
+              staticType = typePool.type(
+                  staticType.typeSpecification.arrayOf(),
+                  formal.getSourcePosition(), logger);
+            }
+            String tdesc = staticType.toDescriptor();
+            if (tdesc.endsWith(errorDesc)) {  // [X is an array of errors.
+              hasErrorType = true;
+              break;
+            }
+            descriptor.append(tdesc);
+          }
+          descriptor.append(')');
+        }
+        if (!hasErrorType) {
+          String rdesc = returnType.toDescriptor();
+          if (rdesc.endsWith(errorDesc)) {
+            hasErrorType = true;
+          } else {
+            descriptor.append(rdesc);
+          }
+        } else {
+          error(node, "Could not compute method descriptor");
+        }
+
+        if (!hasErrorType) {
+          info.setDescriptor(descriptor.toString());
+        }
+        info.setReturnType(returnType);
+        cd.setMemberInfo(info);
       } else {
         error(node, "Missing member info for " + methodName);
       }
@@ -92,10 +186,12 @@ final class ClassMemberPass extends AbstractPass<Void> {
         if (ident != null) {
           Name fieldName = childTypeInfo.canonName.child(
               ident.getValue(), Name.Type.FIELD);
-          CallableInfo info = (CallableInfo) memberWithName(
+          FieldInfo info = (FieldInfo) memberWithName(
               childTypeInfo, fieldName);
           if (info != null) {
-            // TODO: store the type with the field info.
+            WholeType valueType = findWholeType(node);
+            info.setValueType(valueType.getStaticType());
+            ((MemberDeclaration) node).setMemberInfo(info);
           } else {
             error(node, "Missing member info for " + fieldName);
           }
@@ -104,6 +200,29 @@ final class ClassMemberPass extends AbstractPass<Void> {
     }
     // TODO annotation elements
 
+  }
+
+  private WholeType findWholeType(BaseNode node) {
+    WholeType wholeType = null;
+    for (WholeType t
+        : node.finder(WholeType.class)
+        .exclude(NodeType.Throws, NodeType.TypeParameters,
+            NodeType.MethodBody, NodeType.ConstructorBody,
+            NodeType.BlockStatements,
+            NodeType.VariableInitializer,
+            NodeType.FormalParameterList, NodeType.Result,
+            NodeType.Type)
+        .find()) {
+      if (wholeType != null) {
+        error(
+            t,
+            "Duplicate type at "
+            + wholeType.getSourcePosition());
+      } else {
+        wholeType = t;
+      }
+    }
+    return wholeType;
   }
 
   private static MemberInfo memberWithName(TypeInfo typeInfo, Name name) {
@@ -116,7 +235,7 @@ final class ClassMemberPass extends AbstractPass<Void> {
   }
 
   StaticType toStaticType(BaseNode node, TypeNameResolver r) {
-    Class<?> delegateType = null;
+    Class<? extends BaseNode> delegateType = null;
     switch (node.getNodeType()) {
       case Result: {
         ResultNode.Variant v = ((ResultNode) node).getVariant();
@@ -218,6 +337,7 @@ final class ClassMemberPass extends AbstractPass<Void> {
                 IdentifierNode.class);
             Name name = Name.root(ident.getValue(), Name.Type.TYPE_PARAMETER);
             ImmutableList<Name> canonNames = r.lookupTypeName(name);
+            System.err.println("name=" + name + ", canonNames=" + canonNames);
             switch (canonNames.size()) {
               case 0:
                 error(ident, "Undefined type variable name " + name);
@@ -260,20 +380,31 @@ final class ClassMemberPass extends AbstractPass<Void> {
           case NotAtType:
             delegateType = TypeNode.class;
             break;
-
+        }
+        break;
+      }
+      case Type: {
+        TypeNode.Variant v = ((TypeNode) node).getVariant();
+        switch (v) {
+          case PrimitiveType:
+            delegateType = PrimitiveTypeNode.class;
+            break;
+          case ReferenceType:
+            delegateType = ReferenceTypeNode.class;
+            break;
         }
         break;
       }
       default:
         break;
     }
-    if (delegateType == null) {
-      BaseNode delegate = node.firstChildWithType(UnannTypeNode.class);
+    if (delegateType != null) {
+      BaseNode delegate = node.firstChildWithType(delegateType);
       if (delegate != null) {
         return toStaticType(delegate, r);
       }
     }
-    error(node, "Malformed type");
+    error(node, "Malformed type for node of type " + node.getNodeType());
     return StaticType.ERROR_TYPE;
   }
 
---
+++ src/test/java/com/mikesamuel/cil/ast/passes/ClassMemberPassTest.java
@@ -0,0 +1,115 @@
+package com.mikesamuel.cil.ast.passes;
+
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.mikesamuel.cil.ast.BaseNode;
+import com.mikesamuel.cil.ast.CompilationUnitNode;
+import com.mikesamuel.cil.ast.Java8Comments;
+import com.mikesamuel.cil.ast.Trees.Decorator;
+import com.mikesamuel.cil.ast.meta.MemberInfo;
+import com.mikesamuel.cil.ast.meta.StaticType.TypePool;
+import com.mikesamuel.cil.ast.meta.TypeInfoResolver;
+import com.mikesamuel.cil.ast.passes.PassTestHelpers.PassRunner;
+import com.mikesamuel.cil.ast.traits.MemberDeclaration;
+import com.mikesamuel.cil.parser.Unparse.UnparseVerificationException;
+
+import junit.framework.TestCase;
+
+@SuppressWarnings("javadoc")
+public final class ClassMemberPassTest extends TestCase {
+
+  private static void assertMembers(
+      String[][] expected,
+      String[][] inputs,
+      String... expectedErrors)
+  throws UnparseVerificationException {
+    PassTestHelpers.assertAnnotatedOutput(
+        new PassRunner() {
+
+          @Override
+          public ImmutableList<CompilationUnitNode> runPasses(
+              Logger logger, ImmutableList<CompilationUnitNode> cus) {
+            DeclarationPass declPass = new DeclarationPass(logger);
+            TypeInfoResolver typeInfoResolver = declPass.run(cus);
+            DisambiguationPass disambigPass = new DisambiguationPass(
+                typeInfoResolver, logger, false);
+            ImmutableList<CompilationUnitNode> rewritten =
+                disambigPass.run(cus);
+            TypePool typePool = new TypePool(typeInfoResolver);
+            ClassMemberPass classMemberPass = new ClassMemberPass(
+                logger, typePool);
+            classMemberPass.run(rewritten);
+            return rewritten;
+          }
+
+        },
+        expected,
+        inputs,
+        DECORATOR,
+        expectedErrors);
+  }
+
+  @Test
+  public static void testMembers() throws Exception {
+    assertMembers(
+        new String[][] {
+          {
+            "class C {",
+            "  /* (FieldInfo static final /C.X : int) */",
+            "  static final int X;",
+            "  /* (FieldInfo private /C.x : int) */",
+            "  private int x;",
+            "  /* (CallableInfo private static /C.<clinit>(1) @ ()V : void) */",
+            "  static{ X = 42; }",
+            "  /* (CallableInfo /C.<init>(1) @ ()V : void) */",
+            "  C() { x = X; }",
+            "  /* (CallableInfo /C.<init>(2) @ (Ljava/lang/String;)V : void) */",
+            "  C(String s) { x = X; }",
+            "  /* (CallableInfo /C.<init>(3) @ ([Ljava/lang/Integer;)V : void) */",
+            "  C(Integer... is) { x = X; }",
+            "  /* (CallableInfo /C.f(1) @ (Ljava/lang/Integer;)I : int) */",
+            "  int f(Integer i) { return i; }",
+            "  /* (CallableInfo /C.f(2) @ (Ljava/lang/String;)Ljava/lang/String; : /C.f(2).<T>) */",
+            "  <T extends String> T f(T s) { return s; }",
+            "  /* (CallableInfo /C.f(3) @ (Ljava/lang/Object;)Ljava/lang/Object; : /C.f(3).<T>) */",
+            "  <T> T f(T o) { return o; }",
+            "}",
+          },
+        },
+        new String[][] {
+          {
+            "//C",
+            "class C {",
+            "  static final int X;",
+            "  private int x;",
+            "  static { X = 42; }",
+            "  C() { x = X; }",
+            "  C(String s) { x = X; }",
+            "  C(Integer... is) { x = X; }",
+            "  int f(Integer i) { return i; }",
+            "  <T extends String> T f(T s) { return s; }",
+            "  <T> T f(T o) { return o; }",
+            "}",
+          },
+        });
+  }
+
+  private static final Decorator DECORATOR = new Decorator() {
+
+    @Override
+    public String decorate(BaseNode node) {
+      if (node instanceof MemberDeclaration) {
+        MemberDeclaration md = (MemberDeclaration) node;
+        MemberInfo mi = md.getMemberInfo();
+        return Java8Comments.blockComment(
+            mi != null ? mi.toString() : "?",
+            false);
+      }
+      return null;
+    }
+
+  };
+}
