3e3e570cc4d3271a12c6985af3343063eb0b35a5
Mike Samuel <mikesamuel@gmail.com>
2017-07-07 13:24:00 -0400

    added method to map java.lang.reflect Modifier bits to ModifierNode variants

--- src/main/java/com/mikesamuel/cil/ast/passes/ModifierNodes.java
+++ src/main/java/com/mikesamuel/cil/ast/passes/ModifierNodes.java
@@ -2,6 +2,9 @@ package com.mikesamuel.cil.ast.passes;
 
 import java.lang.reflect.Modifier;
 
+import javax.annotation.Nullable;
+
+import com.google.common.base.Preconditions;
 import com.mikesamuel.cil.ast.j8.ModifierNode;
 
 final class ModifierNodes {
@@ -22,5 +25,24 @@ final class ModifierNodes {
       case Volatile:     return Modifier.VOLATILE;
     }
     throw new AssertionError(modVariant);
+  }
+
+  public static @Nullable ModifierNode.Variant modifierVariant(int bit) {
+    Preconditions.checkArgument(bit != 0 && (bit & (bit - 1)) == 0);
+    switch (bit) {
+      case Modifier.ABSTRACT:     return ModifierNode.Variant.Abstract;
+      case Modifier.FINAL:        return ModifierNode.Variant.Final;
+      case Modifier.INTERFACE:    return null;
+      case Modifier.NATIVE:       return ModifierNode.Variant.Native;
+      case Modifier.PRIVATE:      return ModifierNode.Variant.Private;
+      case Modifier.PROTECTED:    return ModifierNode.Variant.Protected;
+      case Modifier.PUBLIC:       return ModifierNode.Variant.Public;
+      case Modifier.STATIC:       return ModifierNode.Variant.Static;
+      case Modifier.STRICT:       return ModifierNode.Variant.Strictfp;
+      case Modifier.SYNCHRONIZED: return ModifierNode.Variant.Synchronized;
+      case Modifier.TRANSIENT:    return ModifierNode.Variant.Transient;
+      case Modifier.VOLATILE:     return ModifierNode.Variant.Volatile;
+    }
+    throw new IllegalArgumentException(Modifier.toString(bit));
   }
 }
