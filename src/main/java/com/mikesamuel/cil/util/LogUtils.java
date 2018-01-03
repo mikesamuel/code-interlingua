96de6239a1760ccb4ee21a07e07b56f8232a2086
Mike Samuel <mikesamuel@gmail.com>
2018-01-02 22:49:37 -0500

    make it easy to render nodes for error messages

--- src/main/java/com/mikesamuel/cil/util/LogUtils.java
+++ src/main/java/com/mikesamuel/cil/util/LogUtils.java
@@ -5,9 +5,19 @@ import java.util.logging.Logger;
 
 import javax.annotation.Nullable;
 
+import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
+import com.mikesamuel.cil.ast.Trees;
+import com.mikesamuel.cil.ast.j8.J8BaseNode;
+import com.mikesamuel.cil.event.Event;
 import com.mikesamuel.cil.parser.Positioned;
+import com.mikesamuel.cil.parser.SList;
+import com.mikesamuel.cil.parser.SerialErrorReceiver;
+import com.mikesamuel.cil.parser.SerialState;
 import com.mikesamuel.cil.parser.SourcePosition;
+import com.mikesamuel.cil.parser.Unparse;
+import com.mikesamuel.cil.parser.Unparse.UnparseVerificationException;
+import com.mikesamuel.cil.parser.Unparse.Verified;
 
 /**
  * Utilities for java.util.logging.
@@ -44,5 +54,27 @@ public class LogUtils {
       @Nullable Positioned p, String message) {
     SourcePosition pos = p != null ? p.getSourcePosition() : null;
     return pos != null ? pos + ": " + message : message;
+  }
+
+  /** Best effort to format the given AST.  Output is not length limited. */
+  public static String serialize(J8BaseNode node) {
+    SList<Event> events = Trees.startUnparse(null, node, null);
+    Optional<SerialState> sstate = node.getVariant().getParSer().unparse(
+        new SerialState(SList.forwardIterable(events)),
+        SerialErrorReceiver.DEV_NULL);
+    if (sstate.isPresent()) {
+      Verified verified;
+      try {
+        verified = Unparse.verify(
+            SList.forwardIterable(sstate.get().output));
+      } catch (@SuppressWarnings("unused")
+               UnparseVerificationException e) {
+        verified = null;
+      }
+      if (verified != null) {
+        return Unparse.format(verified).code;
+      }
+    }
+    return "<unprintable " + node.getNodeType() + ">";
   }
 }
